48a0cefc0ec5eeb2e57178f06b7cde3925a3d436
jayendra-sharan <jayendra.sharan@gmail.com>
2016-03-08 10:34:47 +0530

    updated the cart page

--- js/custom_handles.js
+++ js/custom_handles.js
@@ -1,3 +1,18 @@
 Handlebars.registerHelper('uppercase', function(str) {
   return str.toUpperCase();
 });
+
+Handlebars.registerHelper('ifCond', function(o1, o2, options) {
+  if(o1 < o2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+Handlebars.registerHelper('strikethrough', function(str) {
+  return new Handlebars.SafeString ("<p class='discount'>" + str + "</p>");
+});
+
+Handlebars.registerHelper('currency', function(str) {
+  return new Handlebars.SafeString('<span class="dollar_sign">$</span>'+(('0' + str.toFixed(2)).slice(-5)).toString());});
+
--- js/script.js
+++ js/script.js
@@ -11,7 +11,54 @@
 				var source = $("#my-cart-template").html();
 				var template = Handlebars.compile(source);
 				$("#my-cart").append(template(context));
+
+				
+				var subTotalAmount = calculateSubTotal(context);
+				var discountAmount = calculateDiscount(context);
+				var promoCodeValue = calculatePromoCode();
+				var shippingAmount = calculateShipping();
+
+				var estimatedTotal = subTotalAmount - discountAmount - promoCodeValue + shippingAmount;
+
+				shippingAmountD = (shippingAmount === 0) ? "FREE" : '<span class="dollar_sign">$</span>'+shippingAmount.toFixed(2); 
+				$(".count").html(context.productsInCart.length);
+				$(".subtotal_amount").html('<span class="dollar_sign">$</span>'+subTotalAmount.toFixed(2));
+				$("._discount_amount").html('-'+'<span class="dollar_sign">$</span>'+(subTotalAmount*discountAmount).toFixed(2));
+				$(".promo_applied_amount").html('<span class="dollar_sign">$</span>'+promoCodeValue.toFixed(2));
+				$(".shipping_amount").html(shippingAmountD);
+				$(".estimated_total_amount").html('<span class="dollar_sign">$</span>'+estimatedTotal.toFixed(2));
+
+				
+
+
 			});
+
+			function calculateSubTotal(obj) {
+				var sum=0;
+				for (var i = 0; i < obj.productsInCart.length; i++) {
+					sum += parseInt(obj.productsInCart[i].p_price);
+				}
+			  return sum;
+			}
+
+			function calculateDiscount(obj){
+				var totalItems = obj.productsInCart.length;
+
+				if (length >= 10 ) {
+					return 0.25;
+				} else if (totalItems < 10 && totalItems > 3) {
+					return 0.10;
+				} else {
+					return 0.05;
+				}
+			}
+
+			function calculatePromoCode() {
+				return 0;
+			}
+			function calculateShipping() {
+				return 0;
+			}
 			
 		},
 
